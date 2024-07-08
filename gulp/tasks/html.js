51f6586d5ae6f1f42a91867998cfacd621735686
SergeiTsaplii <tsaplii98@gmail.com>
2024-07-09 00:47:45 +0300

    create task html

---
+++ gulp/tasks/html.js
@@ -0,0 +1,63 @@
+import pkg from 'gulp';
+import plumber from 'gulp-plumber';
+import fileinclude from 'gulp-file-include';
+import replace from 'gulp-replace';
+import typograf from 'gulp-typograf';
+import avifWebpHtml from 'gulp-avif-webp-retina-html';
+import prettier from 'gulp-prettier';
+import htmlmin from 'gulp-htmlmin';
+import gulpIf from 'gulp-if';
+import config from '../config.js';
+import plumberNotify from './notify.js';
+
+const { src, dest } = pkg;
+
+function html(isBuild, serverInstance) {
+  return src(`${config.src.html}/*.html`)
+    .pipe(plumber(plumberNotify))
+    .pipe(fileinclude({
+      prefix: '@',
+      basepath: '@file'
+    }))
+    .pipe(replace(/<img(?:.|\n|\r)*?>/g, function(match) {
+				return match.replace(/\r?\n|\r/g, '').replace(/\s{2,}/g, ' ');
+    }))
+    .pipe(replace(
+      /(?<=src=|href=|srcset=)(['"])(\.(\.)?\/)*(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
+					'$1./$4$5$7$1'
+    ))
+    .pipe(gulpIf(isBuild, replace('.css', '.min.css')))
+    .pipe(gulpIf(isBuild, replace('.js', '.min.js')))
+    .pipe(typograf({
+      locale: ['ru', 'en-US'],
+      htmlEntity: { type: 'digit' },
+      safeTags: [
+        ['<\\?php', '\\?>'],
+        ['<no-typography>', '</no-typography>'],
+      ],
+    }))
+    .pipe(avifWebpHtml({
+      extensions: ['jpg', 'jpeg', 'png', 'gif', 'webp', 'avif'],
+      retina: {
+        1: '',
+        2: '@2x',
+      },
+    }))
+    .pipe(prettier({
+        printWidth: 182,
+				trailingComma: 'es5',
+				bracketSpacing: false,
+    }))
+    .pipe(htmlmin({
+      useShortDoctype: true,
+      sortClassName: true,
+      removeComments: isBuild,
+
+      /**Раскомментировать если требуется минификация html */
+      collapseWhitespace: isBuild,
+    }))
+    .pipe(dest(`${config.dest.html}`))
+    .pipe(serverInstance.stream());
+};
+
+export default html;
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 import pkg from 'gulp';
 import browserSync from 'browser-sync';
+import config from './gulp/config.js';
 import clean from './gulp/tasks/clean.js';
-import server from './gulp/tasks/server.js'
+import server from './gulp/tasks/server.js';
+import html from './gulp/tasks/html.js';
 
 const { parallel, series, watch } = pkg;
+const isBuild = process.argv.includes('--build');
 const browserSyncInstance = browserSync.create();
 const handleServer = server.bind(null, browserSyncInstance);
+const handleHTML = html.bind(null, isBuild, browserSyncInstance);
 
 function watcher() {
-  watch();
+  watch(`${config.src.html}/**/*.html`, handleHTML);
 };
 
-const dev = series(clean, parallel(watcher, handleServer));
+const dev = series(clean, parallel(handleHTML), parallel(watcher, handleServer));
 const build = series(clean);
 
 export default dev;
